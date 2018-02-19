302926c6796e126c6f36bf6fc91835c3041563bb
Pedro <pedro.melo.albuquerque@gmail.com>
2018-02-19 16:00:36 -0300

    Exemplo 1.12

--- Aula01.R
+++ Aula01.R
@@ -7,7 +7,7 @@ library(tidyverse)
 ################################################################################################
 #Lê os dados
 vinhos.tbl<-readr::read_delim("Data/Vinho.txt",delim=" ", col_names = FALSE)
-glimpse(vinhos.df) 
+glimpse(vinhos.tbl) 
 
 #Renomeia as variáveis
 vinhos.tbl <- vinhos.tbl %>%  
@@ -36,7 +36,7 @@ serie %>% ggplot(aes(x=Data,y=Kilolitros)) +
 ################################################################################################
 
 #Simulando o White Noise
-Data< -create_series(~'2018', 'daily')
+Data<- create_series(~'2018', 'daily')
 X <- tibble(c(0,rnorm(nrow(Data)-1,5,10)))
 
 #Cria a série
@@ -90,3 +90,42 @@ acf(RW.ts, lag.max = 10, type = "covariance")
 acf(RW.ts, lag.max = 10, type = "correlation")
 
 
+################################################################################################
+#################################   Exemplo 1.12    ############################################
+################################################################################################
+
+#Lê os dados
+Strikes.tbl<-readr::read_delim("Data/STRIKES.txt",delim=" ", col_names = TRUE)
+glimpse(Strikes.tbl) 
+
+#Cria a data
+anos <- create_series('1951' ~ '1980', 'yearly')
+Strikes.tbl <-  bind_cols(Strikes.tbl, anos)
+
+#Transforma o objeto em uma série temporal
+serie <- as_tbl_time(Strikes.tbl, index = date) 
+
+#Plotando a série
+serie %>% ggplot(aes(x=date,y=STRIKES)) +
+          geom_line(color="royalblue")
+
+#Decomposição
+STRIKES.ts <- ts(serie$STRIKES, frequency=5)
+STRIKES <- decompose(STRIKES.ts, type = "additive")
+plot(STRIKES)
+
+#Média móvel finita q=2
+serie <- serie %>% 
+         mutate(ma2=rollapply(STRIKES,2,mean,align='right',fill=NA))
+
+#Prepara a base para o plot
+serie.long <- serie %>%
+              gather(STRIKES, ma2, -date)
+serie.long %>% ggplot(aes(x=date, y=ma2, colour=STRIKES,group=STRIKES)) + geom_line()
+
+#Removendo a tendência
+serie <- serie %>% 
+         mutate(Y=STRIKES-ma2)
+serie %>% ggplot(aes(x=date,y=Y)) +
+  geom_line(color="royalblue")+
+  labs(x="Data",y="Y", caption="Exemplo 1.12")
